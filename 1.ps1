2d238d1fbf0491b74b9e4e11481563bcbda066f9
MAKRI <31958699+alimakri@users.noreply.github.com>
2025-11-17 12:55:36 +0100

    12:55

---
+++ 1.ps1
@@ -0,0 +1,24 @@
+﻿$i=12
+$j=$i
+
+
+# Création d'un objet personnalisé avec des propriétés
+$objetVide = @{}
+
+$personne1 = @{
+    Nom    = "Dupont"
+    Prenom = "Jean"
+}
+$personne2 = @{
+    Nom    = "Durand"
+    Prenom = "Isabelle"
+}
+
+
+# Affichage de l'objet
+$p3 = $personne1
+
+
+$p3.Prenom = "Nathalie"
+
+$personne1
---
+++ 2.ps1
@@ -0,0 +1,18 @@
+﻿Get-Service | Where-Object {$_.Status -eq "Running"} | Sort-Object -Property Name -Descending | Measure-Object
+Get-Service | Where-Object {$_.Status -eq "Stopped"} | Sort-Object -Property Name -Descending | Measure-Object
+
+Get-Service | Select-Object -Property Name, Status | gm 
+
+Get-Service | Where-Object {$_.Status -eq "Running"} | Sort-Object -Property Name -Descending
+# equivalent
+$services = Get-Service
+$serviceRunning = $services | Where-Object {$_.Status -eq "Running"} 
+$serviceRunningTrie = $serviceRunning | Sort-Object -Property Name -Descending
+$serviceStppped = $services | Where-Object {$_.Status -eq "Stopped"} 
+
+# extraction de propriété
+Get-Service | Select-Object -ExpandProperty Name | gm
+
+# Intérêt du select
+Get-Service | Where-Object {$_.Status -eq "Running"} | Select -Property Name, DisplayName `
+            | Export-Csv -Path "D:\powershell_20251117"
