5f15d919b553cc0b6e14e6447cbdce4bbc49dc0b
MAKRI <31958699+alimakri@users.noreply.github.com>
2025-11-18 11:01:30 +0100

    11:01

--- 11.ps1
+++ 11.ps1
@@ -1,4 +1,6 @@
-﻿# Créer la connexion
+﻿# Mode connecté
+# -------------
+# Créer la connexion
 $connection = New-Object System.Data.SqlClient.SqlConnection
 $connection.ConnectionString = "Server=.\SQLEXPRESS;Database=AdventureWorks2022;Integrated Security=True;"
 $connection.Open()
@@ -6,12 +8,15 @@ $connection.Open()
 # Exécuter la commande SQL
 $command = New-Object System.Data.SqlClient.SqlCommand
 $command.Connection = $connection
-$command.CommandText = "SELECT ProductID, Name, ProductNumber, ListPrice FROM Production.Product where Name like 'C%'"
+$command.CommandText = "select Top 10 BusinessEntityID, FirstName, LastName from Person.Person"
 
-# Lire les résultats
-$reader = $command.ExecuteReader()
-$table = New-Object System.Data.DataTable
-$table.Load($reader)
+$reader = $command.ExecuteReader()  # Crée un object de type SqlDataReader
 
-# Afficher les résultats
-$table | Format-Table -AutoSize
+while($Reader.Read())
+{
+    $id = $reader["BusinessEntityId"]
+    $prenom = $reader["FirstName"]
+    $nom = $reader["LastName"]
+    Write-Host "$id - $prenom - $nom"
+}
+$reader | gm
